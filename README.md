e86d89cbfa38fe40fec75af7239a0169d5c887db
Delphine Asir <delphineasir@gmail.com>
2024-06-27 18:59:01 +0530

    Model pipeline created

--- README.md
+++ README.md
@@ -74,7 +74,6 @@ python script.py
 Run this to export as env variables:
 
 ```bash
-
 export MLFLOW_TRACKING_URI=https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow
 
 export MLFLOW_TRACKING_USERNAME=entbappy 
--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
-#from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
-
+from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
+   #data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
@@ -39,3 +38,15 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+STAGE_NAME = "Model Evaluation"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
